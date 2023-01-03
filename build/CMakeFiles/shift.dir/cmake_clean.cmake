0c24845b3c7a32d35d6a4279720722f71fce1517
Ark1409 <codeark1409@gmail.com>
2023-01-02 21:01:32 -0800

    cmake setup

---
+++ build/CMakeFiles/shift.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shift.dir/src/main.cpp.obj"
+  "CMakeFiles/shift.dir/src/main.cpp.obj.d"
+  "bin/shift.exe"
+  "bin/shift.exe.manifest"
+  "bin/shift.pdb"
+  "lib/libshift.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shift.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
