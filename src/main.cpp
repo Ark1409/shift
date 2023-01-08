b5d7b248233d1b3c2e1470daee3ae3a795b6a472
Ark1409 <codeark1409@gmail.com>
2023-01-07 22:41:11 -0800

    console test

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
  *
  * Main file
  */
+#include "logging/console.h"
 int main() {
+    shift::logging::enable_colored_console();
+    std::cout << shift::logging::lred << "Red text" << shift::logging::creset << std::endl;
+    shift::logging::disable_colored_console();
     return 0;
 }
