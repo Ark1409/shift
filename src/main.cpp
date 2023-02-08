36a6b75e6176a33888ff92a13b5ac8f301aaee37
Ark1409 <codeark1409@gmail.com>
2023-02-07 20:12:50 -0800

    Changed main to use compiler class

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,19 @@
  * Main file
  */
 #include "logging/console.h"
-#include "compiler/shift_parser.h"
+#include "compiler/shift_compiler.h"
 
 int main() {
     shift::logging::enable_colored_console();
-    std::cout << shift::logging::lred << "Red text" << shift::logging::creset << std::endl;
+    {
+        using namespace shift::compiler;
+        compiler comp({std::string_view("../../test/file.shift")});
+        comp.get_error_handler().enable_warnings();
+        comp.parse_flags();
+        comp.tokenize();
+        comp.parse();
+        comp.get_error_handler().print_clear();
+    }
     shift::logging::disable_colored_console();
     return 0;
 }
