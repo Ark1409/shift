708f97a5fa08f2e34bd04e36115deed1fdc25800
Ark1409 <codeark1409@gmail.com>
2023-02-18 18:39:36 -0800

    Implemented use of analyzer in program

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,9 @@ int main() {
     shift::logging::enable_colored_console();
     {
         using namespace shift::compiler;
-        compiler comp({std::string_view("../../test/file.shift")});
+        compiler comp({"../../test/file.shift", "../../test/test2.shift"});
         comp.get_error_handler().enable_warnings();
-        comp.parse_flags();
-        comp.tokenize();
-        comp.parse();
+        comp.run();
         comp.get_error_handler().print_clear();
     }
     shift::logging::disable_colored_console();
