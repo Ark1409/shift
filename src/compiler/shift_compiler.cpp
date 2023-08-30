e2e698a20c7bc5cdace6a52786b5f09ec57544df
Ark1409 <ark1409dev@gmail.com>
2023-08-29 23:10:18 -0700

    Slight change to shift_compiler to decrease chance of data race

--- src/compiler/shift_compiler.cpp
+++ src/compiler/shift_compiler.cpp
@@ -3,38 +3,35 @@
  */
 #include "compiler/shift_compiler.h"
 
-namespace shift {
-    namespace compiler {
-        void compiler::tokenize() {
-            for (filesystem::file const& file : m_args.get_source_files()) {
-                const auto error_count_begin = m_error_handler.get_error_count();
+namespace shift::compiler {
+    void compiler::tokenize() {
+        for (filesystem::file const& file : m_args.get_source_files()) {
+            const auto error_count_begin = m_error_handler.get_error_count();
 
-                tokenizer _tokenizer(&m_error_handler, file);
-                _tokenizer.tokenize();
+            tokenizer _tokenizer(&m_error_handler, file);
+            _tokenizer.tokenize();
 
-                const auto error_count_end = m_error_handler.get_error_count();
+            const auto error_count_end = m_error_handler.get_error_count();
 
-                if (error_count_begin == error_count_end)
-                    m_tokenizers.push_back(std::move(_tokenizer));
-            }
+            if (error_count_begin == error_count_end)
+                m_tokenizers.push_back(std::move(_tokenizer));
         }
+    }
 
-        void compiler::parse() {
-            for (tokenizer& _tokenizer : m_tokenizers) {
-                const auto error_count_begin = m_error_handler.get_error_count();
+    void compiler::parse() {
+        for (tokenizer& _tokenizer : m_tokenizers) {
+            const auto error_count_begin = m_error_handler.get_error_count();
 
-                m_parsers.emplace_back(&m_error_handler, &_tokenizer);
-                parser& _parser = m_parsers.back();
-                _parser.parse();
+            parser& _parser = m_parsers.emplace_back(&m_error_handler, &_tokenizer);
+            _parser.parse();
 
-                const auto error_count_end = m_error_handler.get_error_count();
-                if (error_count_begin != error_count_end)
-                    m_parsers.erase(--m_parsers.end());
-            }
+            const auto error_count_end = m_error_handler.get_error_count();
+            if (error_count_begin != error_count_end)
+                m_parsers.erase(--m_parsers.end()); // TODO may want a reliable way of obtaining the iterator to the current element if multi-threaded parsing occurs
         }
+    }
 
-        void compiler::analyze() {
-            this->m_analyzer.analyze();
-        }
+    void compiler::analyze() {
+        this->m_analyzer.analyze();
     }
 }
